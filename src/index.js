4da3d0ebc8b844e865e22319f6bd2543ec2c9dd5
beena8457 <beena8457@gmail.com>
2024-09-16 14:20:13 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
-import express from "express";
-const app = express()
 
+import dotenv from "dotenv"
+ import connectDB from "./db/index.js";
 
+dotenv.config({
+    path: './env'
+})
 
+connectDB()
 
 
 
 /*
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+import express from "express";
+
+const app = express()
 (async () => {
     try {
       await mongoose.connect('${process.env.MONGO_URL}/${DB_NAME}') 
